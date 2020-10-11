e01e96d60b5bf53dadcf169bf261f65a2d5312d9
MathStPierre <mathieu.st-pierre@curbside.com>
2020-10-11 10:39:08 -0400

    Add tests for setTrackingInfo and set user info

--- plugin/tests/tests.js
+++ plugin/tests/tests.js
@@ -18,65 +18,85 @@
  * under the License.
  *
  */
-
 exports.defineAutoTests = function () {
-    describe('Device Information (window.device)', function () {
+    describe('Curbside', function () {
+
         it('should exist', function () {
-            expect(window.device).toBeDefined();
+            expect(window.Curbside).toBeDefined();
         });
 
-        it('should contain a platform specification that is a string', function () {
-            expect(window.device.platform).toBeDefined();
-            expect(String(window.device.platform).length > 0).toBe(true);
+        it('Call setTrackingIdentifier', function () {
+            var err = null
+            window.Curbside.setTrackingIdentifier("CORDOVA_PLUGIN_TEST_ID", function(error){
+                err = error;
+            });
+
+            expect(err).toBeNull();
         });
 
-        it('should contain a version specification that is a string', function () {
-            expect(window.device.version).toBeDefined();
-            expect(String(window.device.version).length > 0).toBe(true);
+        it('Call setUserInfo', function () {
+            var err = null
+
+            var obj = JSON.parse('{ "fullName":"Denis Doe", "emailAddress":"denis.doe@rakuten.com", "smsNumber":"730-412-3021", "vehicleMake":"Chevrolet","vehicleModel":"Corvette","vehicleLicensePlate":"RAKREADY" }');
+            window.Curbside.setUserInfo(obj, function(error){
+                err = error;
+            });
+
+            expect(err).toBeNull();
         });
 
-        it('should contain a UUID specification that is a string or a number', function () {
-            expect(window.device.uuid).toBeDefined();
-            if (typeof window.device.uuid === 'string' || typeof window.device.uuid === 'object') {
-                expect(String(window.device.uuid).length > 0).toBe(true);
-            } else {
-                expect(window.device.uuid > 0).toBe(true);
-            }
-        });
+        // it('should contain a platform specification that is a string', function () {
+        //     expect(window.device.platform).toBeDefined();
+        //     expect(String(window.device.platform).length > 0).toBe(true);
+        // });
 
-        it('should contain a cordova specification that is a string', function () {
-            expect(window.device.cordova).toBeDefined();
-            expect(String(window.device.cordova).length > 0).toBe(true);
-        });
+        // it('should contain a version specification that is a string', function () {
+        //     expect(window.device.version).toBeDefined();
+        //     expect(String(window.device.version).length > 0).toBe(true);
+        // });
 
-        it('should depend on the presence of cordova.version string', function () {
-            expect(window.cordova.version).toBeDefined();
-            expect(String(window.cordova.version).length > 0).toBe(true);
-        });
+        // it('should contain a UUID specification that is a string or a number', function () {
+        //     expect(window.device.uuid).toBeDefined();
+        //     if (typeof window.device.uuid === 'string' || typeof window.device.uuid === 'object') {
+        //         expect(String(window.device.uuid).length > 0).toBe(true);
+        //     } else {
+        //         expect(window.device.uuid > 0).toBe(true);
+        //     }
+        // });
 
-        it('should contain device.cordova equal to cordova.version', function () {
-            expect(window.device.cordova).toBe(window.cordova.version);
-        });
+        // it('should contain a cordova specification that is a string', function () {
+        //     expect(window.device.cordova).toBeDefined();
+        //     expect(String(window.device.cordova).length > 0).toBe(true);
+        // });
 
-        it('should contain a model specification that is a string', function () {
-            expect(window.device.model).toBeDefined();
-            expect(String(window.device.model).length > 0).toBe(true);
-        });
+        // it('should depend on the presence of cordova.version string', function () {
+        //     expect(window.cordova.version).toBeDefined();
+        //     expect(String(window.cordova.version).length > 0).toBe(true);
+        // });
 
-        it('should contain a manufacturer property that is a string', function () {
-            expect(window.device.manufacturer).toBeDefined();
-            expect(String(window.device.manufacturer).length > 0).toBe(true);
-        });
+        // it('should contain device.cordova equal to cordova.version', function () {
+        //     expect(window.device.cordova).toBe(window.cordova.version);
+        // });
 
-        it('should contain an isVirtual property that is a boolean', function () {
-            expect(window.device.isVirtual).toBeDefined();
-            expect(typeof window.device.isVirtual).toBe('boolean');
-        });
+        // it('should contain a model specification that is a string', function () {
+        //     expect(window.device.model).toBeDefined();
+        //     expect(String(window.device.model).length > 0).toBe(true);
+        // });
 
-        it('should contain a serial number specification that is a string', function () {
-            expect(window.device.serial).toBeDefined();
-            expect(String(window.device.serial).length > 0).toBe(true);
-        });
+        // it('should contain a manufacturer property that is a string', function () {
+        //     expect(window.device.manufacturer).toBeDefined();
+        //     expect(String(window.device.manufacturer).length > 0).toBe(true);
+        // });
+
+        // it('should contain an isVirtual property that is a boolean', function () {
+        //     expect(window.device.isVirtual).toBeDefined();
+        //     expect(typeof window.device.isVirtual).toBe('boolean');
+        // });
+
+        // it('should contain a serial number specification that is a string', function () {
+        //     expect(window.device.serial).toBeDefined();
+        //     expect(String(window.device.serial).length > 0).toBe(true);
+        // });
     });
 };
 
@@ -97,18 +117,26 @@ exports.defineManualTests = function (contentEl, createActionButton) {
     };
 
     var device_tests =
-        '<h3>Press Dump Device button to get device information</h3>' +
-        '<div id="dump_device"></div>' +
+        '<h3>Press SetTracking Device button to SetTracking</h3>' +
+        '<div id="set_tracking"></div>' +
         'Expected result: Status box will get updated with device info. (i.e. platform, version, uuid, model, etc)';
 
     contentEl.innerHTML = '<div id="info"></div>' + device_tests;
 
     createActionButton(
-        'Dump device',
+        'Set Tracking',
         function () {
-            clearLog();
-            logMessage(JSON.stringify(window.device, null, '\t'));
+
+            // var err1 = null
+            // Curbside.setTrackingIdentifier("CORDOVA_PLUGIN_TEST_ID", function(error){
+            //     err = error;
+            // });
+
+            // // expect(err).toBeNull();
+
+            // clearLog();
+            // logMessage("error : " + err);
         },
-        'dump_device'
+        'set_tracking'
     );
 };
